79ce2e5e33a2d0389dc7e34362fdea63d653f4b6
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-05-08 01:29:41 +0500

    Refactoring

---
+++ Assets/BigHoliday/Code/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+
+namespace BigHoliday
+{
+    internal sealed class PlayerController
+    {
+        #region Fields
+
+        internal Vector3 Position;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        internal PlayerController()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
