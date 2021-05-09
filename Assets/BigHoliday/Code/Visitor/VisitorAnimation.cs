533f73a57d07273ead6a142a042134a78ba4b890
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2021-05-09 17:06:10 +0500

    Visitors update

---
+++ Assets/BigHoliday/Code/Visitor/VisitorAnimation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+
+
+namespace BigHoliday
+{
+    internal sealed class VisitorAnimation : MonoBehaviour, IChangeableStates
+    {
+        #region Properties
+
+        public SpriteRenderer SpriteRenderer { get; private set; }
+
+        public bool IsLooped { get; private set; }
+
+        public event Action<AnimState> OnStateChange;
+
+        #endregion
+
+
+        #region Methods
+
+        public void SetupAnimation(SpriteRenderer spriteRenderer, bool isLooped)
+        {
+            SpriteRenderer = spriteRenderer;
+            IsLooped = isLooped;
+        }
+
+        public void ChangeState(AnimState state)
+        {
+            OnStateChange.Invoke(state);
+        }
+
+        #endregion
+    }
+}
