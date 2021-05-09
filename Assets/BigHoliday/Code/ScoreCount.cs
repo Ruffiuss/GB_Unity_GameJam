0dedb9acd89fddfa571a883adaf76a46a79f4c3d
RomanMaksimov3 <romamaksimov7@bk.ru>
2021-05-09 16:49:20 +0300

    set size for vizitors

---
+++ Assets/BigHoliday/Code/ScoreCount.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour
+{
+     public int Score = 0;
+     public GameObject ScoreText;
+
+     public void Count()
+     {
+          Score++;
+          ScoreText.GetComponent<Text>().text = "Score: " + Score.ToString();
+     }
+}
